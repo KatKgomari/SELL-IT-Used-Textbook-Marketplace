ddc6b220c249b1fbbd973430055b94cd45f782f2
Katlego Kgomari <kgomari88@gmail.com>
2026-02-26 16:35:30 +0200

    made form to make a simple listing. No user authentication involved yet

--- frontend/sell-it/src/pages/Sell/SellForm.tsx
+++ frontend/sell-it/src/pages/Sell/SellForm.tsx
@@ -1,8 +1,70 @@
 import React from "react";
+import { useState } from "react";
 import "./SellForm.css"
 
 const SellForm : React.FC =() =>{
-    return (<div className="sell-form">Sell</div>)
+    const [sellFormData, setSellFormData] = useState({
+        title: "",
+        authors: "",
+        isbn: "",
+        price: 0,
+    })
+
+    const HandleChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        if(e.target.name === "price"){
+            setSellFormData({
+                ...sellFormData,
+                [e.target.name]: e.target.value? Number(e.target.value): 0
+            })
+        }
+        else{
+            setSellFormData({
+                ...sellFormData,
+                [e.target.name]: e.target.value
+            })
+        }
+    }
+
+    const HandleSellFormSubmit = (e:React.SubmitEvent) =>{
+        e.preventDefault() //prevents page refresh
+        console.log(sellFormData)
+    }
+
+    return (
+        <div className="sell-div">
+            <form onSubmit= {HandleSellFormSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Title"
+                    name="title"
+                    value= {sellFormData.title}
+                    onChange= {HandleChange}
+                />
+                <input 
+                    type="text" 
+                    placeholder="Authors"
+                    name= "authors"
+                    value= {sellFormData.authors}
+                    onChange= {HandleChange}
+                    />
+                <input 
+                    type="text" 
+                    placeholder="ISBN"
+                    name="isbn"
+                    value = {sellFormData.isbn}
+                    onChange= {HandleChange}
+                    />
+                <input 
+                    type="text" 
+                    placeholder="0.00"
+                    name= "price"
+                    value={sellFormData.price}
+                    onChange= {HandleChange}
+                />
+                <button type="submit" >Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default SellForm;
